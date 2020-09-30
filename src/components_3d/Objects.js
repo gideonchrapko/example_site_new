ca8f99740b6fc5a2356ebc303df8b4b3d651f612
Gideon Chrapko <gideonchrapko@hotmail.ca>
2020-09-30 14:14:34 -0700

    try to fix black screen

--- src/components_3d/Objects.js
+++ src/components_3d/Objects.js
@@ -19,9 +19,6 @@ const Objects = ({ click }) => {
         rotation[1] += 1;
         set({ rotation: [...rotation] });
     }
-
-    console.log(click);
-
     return (
         <>
             {/* <a.group> */}
@@ -44,49 +41,4 @@ const Objects = ({ click }) => {
     )
 }
 
-
 export default Objects
-
-
-// import React from 'react';
-// import Suzanne from './Suzanne';
-// // import Shirt from './Shirt'
-// import { a, useSpring } from 'react-spring/three';
-
-
-
-// const Objects = ({ rotation }) => {
-//     const animatedProps = useSpring ({
-//       clicked: rotation ? [0, 0.1, 0] : [0, 2, 0],
-//     });
-
-//     const index = rotation.indexOf(2)
-//     if (index !== -1) {
-//         rotation[index] = 1010;
-//     }
-
-//     return (
-//         <>
-//         <a.group 
-//             rotation={animatedProps.clicked} 
-//         >
-//             <Suzanne
-//                 position={[0, -1, -5]}
-//                 rotation={[0, 3, 0]}
-//             />
-//             {/* <Shirt /> */}
-//             <Suzanne 
-//                 position={[4, -1, 2]}
-//                 rotation={[0, 1, 0]}
-//             />
-//             <Suzanne
-//                 position={[-5, -1, 2]}
-//                 rotation={[0, 5, 0]}
-//             />
-//         </a.group>
-//         </>
-//     )
-// }
-
-
-// export default Objects
